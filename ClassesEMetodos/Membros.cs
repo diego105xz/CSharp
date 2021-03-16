3cfa1c39f1857c7c45b5fd3f043b88d68f62aee0
Diego Melo <diego-melo@uni9.edu.br>
2021-03-15 21:27:25 -0300

    Exercicios CursoCod3r

---
+++ ClassesEMetodos/Membros.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class Membros
+    {
+        public static void Executar()
+        {
+            Pessoa sicrano = new Pessoa();
+            sicrano.Nome = "Reanto";
+            sicrano.Idade = 21;
+
+            //Console.WriteLine($"{sicrano.Nome} Tem {sicrano.Idade} anos.");
+
+            sicrano.ApresentarNoConsole();
+            sicrano.Zerar();
+            sicrano.ApresentarNoConsole();
+
+            var fulano = new Pessoa();
+            fulano.Nome = "Beto";
+            fulano.Idade = 21;
+
+            var ApresentacaoDoFulano = fulano.Apresentar();
+            Console.WriteLine(ApresentacaoDoFulano.Length);
+            Console.WriteLine(ApresentacaoDoFulano);
+        }
+    }
+}
