e306f7bb443da5e4a7e19fa2b34666bcbe9c0282
Diego Melo <diego-melo@uni9.edu.br>
2021-03-16 23:41:40 -0300

    Exercicios do curso Cod3r

---
+++ ClassesEMetodos/ParametrosNomeados.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class ParametrosNomeados
+    {
+        public static void Formatar(int dia, int mes, int ano)
+        {   //D2 significa 2 digitos, caso o número não tem 2 digitos será substituido por zero.
+            Console.WriteLine("{0:D2}/{1:D2}/{2}", dia, mes, ano);
+        }
+        public static void Executar()
+        {
+            Formatar(mes: 1, dia: 6, ano: 1996);
+        }
+    }
+}
